6feef18d61fe95c48fc48ba77ce0166266b311e3
Alexia Young <alexia.kwok@gmail.com>
2017-11-24 13:28:48 -0600

    counter working using only React

--- src/App.js
+++ src/App.js
@@ -3,16 +3,34 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: 0,
+    };
+  }
+
+  _handleIncrement() {
+    this.setState({ counter: this.state.counter + 1 })
+  }
+
+  _handleDecrement() {
+    this.setState({ counter: this.state.counter - 1 })
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Counter with Redux</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+        <p className="counter-label">
+          Counter:
         </p>
+        <p className="counter">{this.state.counter}</p>
+        <button className="counter-btn" onClick={this._handleIncrement.bind(this)}>Increment</button>
+        <button className="counter-btn" onClick={this._handleDecrement.bind(this)}>Decrement</button>
       </div>
     );
   }
